d57d2d0f45128de0292c56bd1fdaf20b47bd01af
alexis sae an <alexis.sae.an@gmail.com>
2021-05-18 16:22:00 +0900

    app works

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@articles = 10.times { Article.create([title: Faker::Fantasy::Tolkien.character, content: Faker::Fantasy::Tolkien.poem ]) }
+@articles.save
